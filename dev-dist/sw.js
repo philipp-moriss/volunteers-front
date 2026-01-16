57a5c85d4c1249cf59189503f9f62efe7ed8986c
VanoKi <kirsvano@gmail.com>
2026-01-16 17:20:56 +0300

    feat: create hook (2026-01-16 17:20:56)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-79fb5871'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hinr6m0f8r8"
+    "revision": "0.59k4od5e08"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/tasks/ui/tasks-page.tsx
+++ src/pages/tasks/ui/tasks-page.tsx
@@ -1,8 +1,8 @@
-import { FC, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { TaskList } from '@/widgets/task-list';
-import { Header } from '@/shared/ui';
-import { useGetPrograms } from '@/entities/program/hook/useGetPrograms';
+import {FC, useState} from 'react';
+import {useTranslation} from 'react-i18next';
+import {TaskList} from '@/widgets/task-list';
+import {Header} from '@/shared/ui';
+import {useGetTasks} from "@/entities/task/hook";
 
 export const TasksPage: FC = () => {
   const {t} = useTranslation();
@@ -10,33 +10,20 @@ export const TasksPage: FC = () => {
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
 
-  const { data: programs = [], isLoading: isLoadingPrograms } = useGetPrograms();
+  const { data: tasks = [], isLoading: isLoading } = useGetTasks();
+
+
 
 
   return (
     <div className="min-h-screen bg-gray-50">
       <Header
         title={t('tasks.title')}
-        rightActions={[
-          <select
-            key="program-select"
-            value={selectedProgramId}
-            onChange={(e) => setSelectedProgramId(e.target.value)}
-            className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary bg-white text-sm min-w-[180px] cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
-            disabled={isLoadingPrograms}
-          >
-            <option value="">{t('tasks.allPrograms') || 'All Programs'}</option>
-            {programs.map((program) => (
-              <option key={program.id} value={program.id}>
-                {program.name}
-              </option>
-            ))}
-          </select>,
-        ]}
+        // rightActions={}
       />
-      <Tabs tabs={tabs} onChange={setActiveTab}/>
+      {/*<Tabs tabs={tabs} onChange={setActiveTab}/>*/}
       <div className="px-4 py-6">
-        <TaskList tasks={[]} />
+        <TaskList tasks={tasks} />
       </div>
     </div>
   );
